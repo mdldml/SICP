e9ef2b58638ff1a652fa055b1e5ec3f38644e234
mdldml <mdldml@gmail.com>
2016-03-21 18:39:24 +0300

    fixed tests for ex1.03

--- ch1/ex1.03-test.scm
+++ ch1/ex1.03-test.scm
@@ -3,3 +3,9 @@
 (= (sqr 2) 4)
 (= (sqr 1) 1)
 
+(= (sqrsum 3 4) (sqr 5))
+(= (sqrsum 10 6) 136)
+
+(= (greatest-sqrsum 0 0 0) 0)
+(= (greatest-sqrsum 1 2 3) (greatest-sqrsum 2 3 1))
+(= (greatest-sqrsum 3 4 0) 25)
